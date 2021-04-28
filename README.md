154d3ba687563f1b3eb796851d69ddf6f16576de
kimyeongseo <oseo0202@naver.com>
2021-04-28 16:47:01 +0900

    Random Number Game

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ day5: D-Day Count
 day6: localStorage.setItem, getItem을 활용하여 데이터를 저장
 
 day8-9: event, localStorage를 활용하여 To Do List 만들기
+
+day10: Make a number guessing game using Javascript
---
+++ day10/index.js
@@ -0,0 +1,39 @@
+const input = document.querySelector(".game_my-number_input");
+const playBtn = document.querySelector(".game_play_btn");
+const gameInfo = document.querySelector(".game_my-number");
+const rangeBar = document.querySelector('.game_range_bar');
+
+
+
+playBtn.addEventListener('click',  (event) => {
+numberGame();
+});
+
+function numberGame(){
+    let rangeNum = rangeBar.value;
+    let randomNum = Math.floor(Math.random() * rangeNum);
+    let myNum = input.value;
+    console.log(randomNum);
+
+    if(myNum == ""){
+        alert("숫자를 입력해주세요");
+    }else{
+        document.querySelector(".game_process_info").innerHTML = `You chose: ${myNum}, the machine chose: ${randomNum}.`
+        if(myNum > rangeNum){
+            alert("범위를 확인해주세요");
+        }else if( myNum < 0){
+            alert("범위를 확인해주세요");
+        }else{
+        if(myNum == randomNum){
+            document.querySelector(".game_result").innerHTML = "You won!"
+        }else{
+            document.querySelector(".game_result").innerHTML = "You lost!"
+        }
+    }
+    }
+
+
+
+
+}
+
